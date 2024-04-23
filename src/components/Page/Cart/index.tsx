39c14bfcd4ba1cee2ec9f7aa607533905299b88a
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-04-23 12:30:03 +0700

    add Cart Page

---
+++ src/components/Page/Cart/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from '../../Header'
+import Footer from '../../Footer'
+
+
+const Cart: React.FC = () => (
+  <>
+    <Header />
+    <>
+      {/* Single Page Header start */}
+      <div className="container-fluid page-header py-5">
+        <h1 className="text-center text-white display-6">Cart</h1>
+        <ol className="breadcrumb justify-content-center mb-0">
+          <li className="breadcrumb-item">
+            <a href="/index.html#">Home</a>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="/public#">Pages</a>
+          </li>
+          <li className="breadcrumb-item active text-white">Cart</li>
+        </ol>
+      </div>
+      {/* Single Page Header End */}
+    </>
+
+
+    <Footer />
+  </>
+)
+
+
+export default Cart
