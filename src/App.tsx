efb4263053a7e385b8c59078a5e814315527bd87
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-04-23 12:31:21 +0700

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,92 @@ import './scss/bootstrap.scss'
 import './fontawesome'
 import './App.css'
 import Home from './components/Home'
-import NoPage from './components/NoPage'
+import NoPage from './components/Page/NoPage'
+import Head from './components/Head'
+import Shop from './components/Shop'
+import Contact from './components/Contact'
+import ShopDetail from './components/ShopDetail'
+import Cart from './components/Page/Cart'
+import Checkout from './components/Page/Checkout'
+import Testimonial from './components/Page/Testimonial'
 
 
 const App: React.FC = () => (
   <BrowserRouter>
-
     <Routes>
-      <Route path="/index.html" element={<Home />} />
+      <Route
+        path="/index.html"
+        element={(
+          <>
+            <Head title="Home | Fruitables - Vegetable Website Template " />
+            <Home />
+          </>
+      )}
+      />
+      <Route
+        path="/shop.html"
+        element={(
+          <>
+            <Head title="Shop | Fruitables - Vegetable Website Template" />
+            <Shop />
+          </>
+        )}
+      />
+      <Route
+        path="/shop-detail.html"
+        element={(
+          <>
+            <Head title="Shop Detail| Fruitables - Vegetable Website Template" />
+            <ShopDetail />
+          </>
+        )}
+      />
+      <Route
+        path="/contact.html"
+        element={(
+          <>
+            <Head title="Contact| Fruitables - Vegetable Website Template" />
+            <Contact />
+          </>
+        )}
+      />
+      <Route
+        path="/testimonial.html"
+        element={(
+          <>
+            <Head title="Testimonial | Fruitables - Vegetable Website Template" />
+            <Testimonial />
+          </>
+        )}
+      />
+      <Route
+        path="/cart.html"
+        element={(
+          <>
+            <Head title="Cart| Fruitables - Vegetable Website Template" />
+            <Cart />
+          </>
+        )}
+      />
+      <Route
+        path="/checkout.html"
+        element={(
+          <>
+            <Head title="Checkout | Fruitables - Vegetable Website Template" />
+            <Checkout />
+          </>
+        )}
+      />
+      <Route
+        path="/404.html"
+        element={(
+          <>
+            <Head title="404 | Page not found" />
+            <NoPage />
+          </>
+)}
+      />
       <Route path="/" element={<Navigate to="/index.html" />} />
-      <Route path="/404.html" element={<NoPage />} />
       <Route path="*" element={<Navigate to="/404.html" />} />
     </Routes>
   </BrowserRouter>
---
+++ src/components/Page/Testimonial/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from '../../Header'
+import Footer from '../../Footer'
+
+
+const Testimonial: React.FC = () => (
+  <>
+    <Header />
+    <>
+      {/* Single Page Header start */}
+      <div className="container-fluid page-header py-5">
+        <h1 className="text-center text-white display-6">Testimonial</h1>
+        <ol className="breadcrumb justify-content-center mb-0">
+          <li className="breadcrumb-item">
+            <a href="/index.html#">Home</a>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="/#">Pages</a>
+          </li>
+          <li className="breadcrumb-item active text-white">Testimonial</li>
+        </ol>
+      </div>
+      {/* Single Page Header End */}
+    </>
+    <Footer />
+  </>
+)
+
+
+export default Testimonial
