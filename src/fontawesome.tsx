b5ea3becda18745c7c907b36eb991de01d49af88
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-04-23 10:51:05 +0700

    update font css

--- src/fontawesome.tsx
+++ src/fontawesome.tsx
@@ -4,5 +4,4 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 //Import các icon mà bạn muốn sử dụng trong từng gói
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
-//Add các icon đã được import vào trong thư viện của bạn
-library.add( fas)
+library.add(fas)
