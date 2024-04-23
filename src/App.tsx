392a5562a1f1772f6c426adbd7a9c876f5122bf6
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-04-23 10:51:10 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './scss/bootstrap.scss'
 import './fontawesome'
 import './App.css'
 import Home from './components/Home'
 
+
 const App: React.FC = () => (
   <BrowserRouter>
+
     <Routes>
       <Route path="/index.html" element={<Home />} />
       <Route path="/" element={<Navigate to="/index.html" />} />
