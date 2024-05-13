d63c476d8fbb067d34c6ef64146f0d405f9936dd
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-05-13 16:34:51 +0700

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from 'react'
 import { Container } from 'react-bootstrap'
-import Topbar from './Topbar'
-import Navibar from './Navibar'
+import TopBar from './TopBar'
+import NaviBar from './NaviBar'
 import Spinner from './Spinner'
 import Search from '../Modal/Search'
 import { ContextModalSearch } from '../context/ContextModalSearch'
@@ -19,9 +19,9 @@ const Header: React.FC = () => {
     <ContextModalSearch.Provider value={valueAdd}>
       <Spinner />
       <header className="container-fluid fixed-top">
-        <Topbar />
+        <TopBar />
         <Container className="px-0">
-          <Navibar />
+          <NaviBar />
         </Container>
       </header>
       {isOpenModal && <Search />}
