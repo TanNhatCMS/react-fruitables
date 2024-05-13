d74d4a7cd74ad4e538e1af5a86444e2073b24b89
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-05-13 16:34:31 +0700

    Create index.tsx

---
+++ src/components/Home/FruitsShop/ProductList/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Container, Row, Col, Nav } from 'react-bootstrap'
+
+function ProductList() {
+  return (
+    <Container>
+      <Row>
+        <Col lg={4} className="text-start">
+          <h1>Our Organic Products</h1>
+        </Col>
+        <Col lg={8} className="text-end">
+          <Nav className="d-inline-flex text-center mb-5">
+            <Nav.Item>
+              <Nav.Link
+                href="#tab-1"
+                className="d-flex m-2 py-2 bg-light rounded-pill active"
+              >
+                All Products
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                href="#tab-2"
+                className="d-flex py-2 m-2 bg-light rounded-pill"
+              >
+                Vegetables
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                href="#tab-3"
+                className="d-flex m-2 py-2 bg-light rounded-pill"
+              >
+                Fruits
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                href="#tab-4"
+                className="d-flex m-2 py-2 bg-light rounded-pill"
+              >
+                Bread
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                href="#tab-5"
+                className="d-flex m-2 py-2 bg-light rounded-pill"
+              >
+                Meat
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default ProductList
