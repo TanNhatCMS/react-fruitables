ab8b88efa201c8ed2b48483c32c6812d2fcb9519
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-05-13 16:34:43 +0700

    Update index.tsx

--- src/components/Header/TopLink/index.tsx
+++ src/components/Header/TopLink/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
 
-const TopLink: React.FC = () => {
-  const values = ['Privacy Policy', 'Terms of Use', 'Sales and Refunds']
-  const link = ['/index.html', '/index.html', '/index.html']
+const TopLink: React.FC = () =>{
+  const menu = [
+    {
+      name: 'Privacy Policy',
+      link: '/index.html',
+      target: '/',
+    },
+    {
+      name: 'Terms of Use',
+      link: '/index.html',
+      target: '/',
+    },
+    {
+      name: 'Sales and Refunds',
+      link: '/index.html',
+      target: ' ',
+    },
+  ]
+
   return (
     <div className="top-link pe-2">
-      {values.map((v, i) => (
-        <a href={link[i]} className="text-white">
-          <small className="text-white mx-2">{v}</small>
-          /
+      {menu.map((v, index) => ( // Adding unique key prop
+        <a key={index} href={v.link} className="text-white">
+          <small className="text-white mx-2">{v.name}</small>
+          {v.target}
         </a>
-      ),
-      )}
+      ))}
     </div>
   )
 }
