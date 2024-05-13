25ac93d55325ad18c4c04cc126cca4b1f3c1db90
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-05-13 17:01:37 +0700

    Create index.tsx

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import { Container } from 'react-bootstrap'
+import TopInfo from '../Header/TopInfo'
+import TopLink from '../Header/TopLink'
+
+const TopBar: React.FC = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerWidth < 992) {
+        if (window.scrollY > 55) {
+          document.querySelector('.fixed-top')?.classList.add('shadow')
+        } else {
+          document.querySelector('.fixed-top')?.classList.remove('shadow')
+        }
+      } else if (window.scrollY > 55) {
+        document.querySelector('.fixed-top')?.classList.add('shadow')
+        document.querySelector('.fixed-top')?.setAttribute('style', 'top: -55px')
+      } else {
+        document.querySelector('.fixed-top')?.classList.remove('shadow')
+        document.querySelector('.fixed-top')?.setAttribute('style', 'top: 0')
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+  return (
+    <Container className="topbar bg-primary d-none d-lg-block">
+      <div className="d-flex justify-content-between">
+        <TopInfo />
+        <TopLink />
+      </div>
+    </Container>
+  )
+}
+
+export default TopBar
