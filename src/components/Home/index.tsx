9a53567694d8e597004443e2da2d633b008b1d9d
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-05-13 20:01:20 +0700

    Update index.tsx

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,12 +3,19 @@ import Header from '../Header'
 import Footer from '../Footer'
 import Hero from './Hero'
 import FeaturesSection from './FeaturesSection'
+import Features from './Featurs'
+// import FruitsShop from './FruitsShop'
+// import Tastimonial from './Tastimonial'
 
 const Home: React.FC = () => (
   <>
     <Header />
     <Hero />
     <FeaturesSection />
+    {/*<FruitsShop />*/}
+    <Features />
+
+    {/*<Tastimonial />*/}
     <Footer />
   </>
 )
