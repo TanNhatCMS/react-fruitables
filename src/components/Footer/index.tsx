e352035f573066d4f2e8e84fdb03c5a775a3ac48
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-05-14 16:29:40 +0700

    Add animate back to top

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,26 +3,50 @@ import Copyright from './Copyright'
 import payment from '../../assets/images/payment.png'
 
 const Footer: React.FC = () => {
-
   useEffect(() => {
+    const backToTopButton = document.querySelector('.back-to-top') as HTMLElement
+
     const handleScroll = () => {
-      const backToTop = document.querySelector('.back-to-top') as HTMLElement
-      if (backToTop) {
-        if (window.scrollY > 300) {
-          backToTop.style.display = 'block'
-        } else {
-          backToTop.style.display = 'none'
-        }
+
+      if (window.scrollY > 300) {
+        backToTopButton.style.display = 'block'
+        backToTopButton.classList.add('fade-in')
+        backToTopButton.classList.remove('fade-out')
+      } else {
+        backToTopButton.style.display = 'none'
+        backToTopButton.classList.add('fade-out')
+        backToTopButton.classList.remove('fade-in')
       }
     }
+
     window.addEventListener('scroll', handleScroll)
     return () => {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
+
   const scrollToTop = () => {
-    document.documentElement.scrollTop = 0
-    document.body.scrollTop = 0
+    const start = window.scrollY
+    const startTime = 'now' in window.performance ? performance.now() : new Date().getTime()
+
+    const scrollDuration = 1500 // duration in ms (1.5 seconds)
+
+    const easeInOutExpo = (t: number): number => {
+      // eslint-disable-next-line no-nested-ternary
+      return t === 0 ? 0 : t === 1 ? 1 : t < 0.5 ? 2**(20 * t - 10) / 2 : (2 - 2**(-20 * t + 10)) / 2
+    }
+    const scroll = () => {
+      const currentTime = 'now' in window.performance ? performance.now() : new Date().getTime()
+      const timeElapsed = currentTime - startTime
+      const progress = Math.min(timeElapsed / scrollDuration, 1)
+      const easedProgress = easeInOutExpo(progress)
+      window.scrollTo(0, start * (1 - easedProgress))
+      if (timeElapsed < scrollDuration) {
+        requestAnimationFrame(scroll)
+      }
+    }
+
+    requestAnimationFrame(scroll)
   }
 
   return (
@@ -45,7 +69,7 @@ const Footer: React.FC = () => {
                 <div className="position-relative mx-auto">
                   <input
                     className="form-control border-0 w-100 py-3 px-4 rounded-pill"
-                    type="number"
+                    type="email"
                     placeholder="Your Email"
                   />
                   <button
@@ -59,31 +83,31 @@ const Footer: React.FC = () => {
               </div>
               <div className="col-lg-3">
                 <div className="d-flex justify-content-end pt-3">
-                  {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
                   <a
-                    className="btn  btn-outline-secondary me-2 btn-md-square rounded-circle"
-                    href="/"
+                    className="btn btn-outline-secondary me-2 btn-md-square rounded-circle"
+                    href="https://twitter.com"
+                    aria-label="Twitter"
                   >
                     <i className="fab fa-twitter" />
                   </a>
-                  {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
                   <a
                     className="btn btn-outline-secondary me-2 btn-md-square rounded-circle"
-                    href="/"
+                    href="https://facebook.com"
+                    aria-label="Facebook"
                   >
                     <i className="fab fa-facebook-f" />
                   </a>
-                  {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
                   <a
                     className="btn btn-outline-secondary me-2 btn-md-square rounded-circle"
-                    href="/"
+                    href="https://youtube.com"
+                    aria-label="YouTube"
                   >
                     <i className="fab fa-youtube" />
                   </a>
-                  {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
                   <a
                     className="btn btn-outline-secondary btn-md-square rounded-circle"
-                    href="/"
+                    href="https://linkedin.com"
+                    aria-label="LinkedIn"
                   >
                     <i className="fab fa-linkedin-in" />
                   </a>
@@ -111,47 +135,23 @@ const Footer: React.FC = () => {
             <div className="col-lg-3 col-md-6">
               <div className="d-flex flex-column text-start footer-item">
                 <h4 className="text-light mb-3">Shop Info</h4>
-                <a className="btn-link" href="/">
-                  About Us
-                </a>
-                <a className="btn-link" href="/">
-                  Contact Us
-                </a>
-                <a className="btn-link" href="/">
-                  Privacy Policy
-                </a>
-                <a className="btn-link" href="/">
-                  Terms &amp; Condition
-                </a>
-                <a className="btn-link" href="/">
-                  Return Policy
-                </a>
-                <a className="btn-link" href="/">
-                  FAQs &amp; Help
-                </a>
+                <a className="btn-link" href="/">About Us</a>
+                <a className="btn-link" href="/">Contact Us</a>
+                <a className="btn-link" href="/">Privacy Policy</a>
+                <a className="btn-link" href="/">Terms &amp; Condition</a>
+                <a className="btn-link" href="/">Return Policy</a>
+                <a className="btn-link" href="/">FAQs &amp; Help</a>
               </div>
             </div>
             <div className="col-lg-3 col-md-6">
               <div className="d-flex flex-column text-start footer-item">
                 <h4 className="text-light mb-3">Account</h4>
-                <a className="btn-link" href="/">
-                  My Account
-                </a>
-                <a className="btn-link" href="/">
-                  Shop details
-                </a>
-                <a className="btn-link" href="/">
-                  Shopping Cart
-                </a>
-                <a className="btn-link" href="/">
-                  Wishlist
-                </a>
-                <a className="btn-link" href="/">
-                  Order History
-                </a>
-                <a className="btn-link" href="/">
-                  International Orders
-                </a>
+                <a className="btn-link" href="/">My Account</a>
+                <a className="btn-link" href="/">Shop details</a>
+                <a className="btn-link" href="/">Shopping Cart</a>
+                <a className="btn-link" href="/">Wishlist</a>
+                <a className="btn-link" href="/">Order History</a>
+                <a className="btn-link" href="/">International Orders</a>
               </div>
             </div>
             <div className="col-lg-3 col-md-6">
@@ -161,7 +161,7 @@ const Footer: React.FC = () => {
                 <p>Email: Example@gmail.com</p>
                 <p>Phone: +0123 4567 8910</p>
                 <p>Payment Accepted</p>
-                <img srcSet="" src={payment} className="img-fluid" alt=""  />
+                <img src={payment} className="img-fluid" alt="Payment Methods" />
               </div>
             </div>
           </div>
@@ -170,13 +170,16 @@ const Footer: React.FC = () => {
       {/* Footer End */}
       <Copyright />
       {/* Back to Top */}
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/anchor-is-valid,jsx-a11y/no-static-element-interactions,jsx-a11y/control-has-associated-label */}
-      <a href="#" className="btn btn-primary border-3 border-primary rounded-circle back-to-top" onClick={scrollToTop}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a
+        href="#"
+        className="btn btn-primary border-3 border-primary rounded-circle back-to-top"
+        onClick={scrollToTop}
+        aria-label="Back to top"
+      >
         <i className="fa fa-arrow-up" />
       </a>
     </>
-
-
   )
 }
 
