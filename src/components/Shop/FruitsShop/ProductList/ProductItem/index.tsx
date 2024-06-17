bd94f469d913af0bd5f9e24861b4d79406d86a01
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-17 23:57:00 +0700

    Update index.tsx

--- src/components/Shop/FruitsShop/ProductList/ProductItem/index.tsx
+++ src/components/Shop/FruitsShop/ProductList/ProductItem/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 
-function ProductItem({type , images, name, describe, url, price }) {
+function ProductItem({item}) {
   return (
         <div className="col-md-6 col-lg-6 col-xl-4">
           <div className="rounded position-relative fruite-item">
             <div className="fruite-img">
               <img
-                src={images}
+                src={item.images}
                 className="img-fluid w-100 rounded-top"
                 alt=""
               />
@@ -16,17 +16,17 @@ function ProductItem({type , images, name, describe, url, price }) {
               className="text-white bg-secondary px-3 py-1 rounded position-absolute"
               style={{ top: 10, left: 10 }}
             >
-              {type}
+              {item.type}
             </div>
             <div className="p-4 border border-secondary border-top-0 rounded-bottom">
-              <h4>{name}</h4>
+              <h4>{item.name}</h4>
               <p>
-                {describe}
+                {item.describe}
               </p>
               <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">${price} / kg</p>
+                <p className="text-dark fs-5 fw-bold mb-0">${item.price} / kg</p>
                 <a
-                  href={url}
+                  href={item.url}
                   className="btn border border-secondary rounded-pill px-3 text-primary"
                 >
                   <i className="fa fa-shopping-bag me-2 text-primary" />{' '}
