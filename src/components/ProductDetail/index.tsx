90fcc48be470c335d21f57cc7c6ac7084fb23530
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-18 02:44:19 +0700

    Create index.tsx

---
+++ src/components/ProductDetail/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+import SinglePageHeader from './SinglePageHeader'
+import SingleProduct from './SingleProduct'
+
+
+const ProductDetail: React.FC = () => {
+
+  return (
+    <>
+      <Header />
+      <SinglePageHeader />
+      <SingleProduct />
+      <Footer />
+    </>
+  )
+}
+
+export default ProductDetail
