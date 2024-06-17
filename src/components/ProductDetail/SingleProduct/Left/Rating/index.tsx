a028d2d81ec586dbff499ff90ce0ada144136336
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-18 02:44:34 +0700

    Create index.tsx

---
+++ src/components/ProductDetail/SingleProduct/Left/Rating/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function StarRating({ rating }) {
+  const fullStars = Math.floor(rating)
+  const partialStar = rating - fullStars
+
+  return (
+    <div className="d-flex mb-4">
+      {[...Array(fullStars)].map((_, index) => (
+        <i key={index} className="fa fa-star text-secondary" />
+      ))}
+      {partialStar > 0 && (
+        <i className="fa fa-star text-secondary" />
+      )}
+      {[...Array(5 - fullStars - (partialStar > 0 ? 1 : 0))].map((_, index) => (
+        <i key={index} className="fa fa-star " />
+      ))}
+    </div>
+  )
+}
+
+export default StarRating
