d17e3fcb7550d13a9092376885c033fcacf07c64
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-19 18:43:51 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,10 @@ import Products from "./components/Products"
 import ProductDetail from "./components/ProductDetail"
 import store from "./app/store"
 import { Provider } from "react-redux"
+import LoginForm from "./components/LoginForm"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Profile from "./components/Profile"
 
 const App = () => (
   <Provider store={store}>
@@ -128,6 +132,28 @@ const App = () => (
               <Shop />
             </>
           )}
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <Head title="Login" />
+              <Header />
+              <LoginForm />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <Head title="Profile " />
+              <Header />
+              <Profile />
+              <Footer />
+            </>
+          }
         />
         <Route path="*" element={<Navigate to="/404.html" />} />
       </Routes>
