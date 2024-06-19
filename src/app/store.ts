32e6d8730eddce8001692a0058eff96ebadf2884
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-19 18:19:34 +0700

    add Redux for Modal

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,14 @@ import loginReducer from "../features/login/loginSlice"
 import userReducer from "../features/user/userSlice"
 import productReducer from "../features/products/productSlice"
 import cartReducer from "../features/cart/cartSlice"
+import modalReducer from "../features/modal/modalSlice"
 
 const store = configureStore({
   reducer: {
     login: loginReducer,
     cart: cartReducer,
     products: productReducer,
+    modal: modalReducer,
     user: userReducer
   },
   devTools: true
--- src/components/Modal/Search/index.tsx
+++ src/components/Modal/Search/index.tsx
@@ -1,68 +1,62 @@
-import type React from 'react'
-import { useCallback, useContext, useEffect } from 'react'
-import Modal from 'react-bootstrap/Modal'
-import { ContextModalSearch } from '../../context/ContextModalSearch'
+import type React from "react"
+import { useCallback, useEffect } from "react"
+import Modal from "react-bootstrap/Modal"
+import { useAppDispatch, useAppSelector } from "../../../app/hooks"
+import { closeModal } from "../../../features/modal/modalSlice"
 
-const Search: React.FC = (props) => {
-  const modalContext = useContext(ContextModalSearch)
-  const { isOpenModal, setIsOpenModal } = modalContext
+const Search: React.FC = () => {
+  const isOpenModal = useAppSelector((state) => state.modal.isOpen)
+  const dispatch = useAppDispatch()
 
   const handleCloseEsc = useCallback(
     (ev: KeyboardEvent) => {
-      if (ev.key === 'Escape') {
-        setIsOpenModal(false)
+      if (ev.key === "Escape") {
+        handleClose()
       }
     },
-    [setIsOpenModal],
+    [dispatch]
   )
 
   useEffect(() => {
-    document.addEventListener('keydown', handleCloseEsc)
-    return () => document.removeEventListener('keydown', handleCloseEsc)
+    document.addEventListener("keydown", handleCloseEsc)
+    return () => document.removeEventListener("keydown", handleCloseEsc)
   }, [handleCloseEsc])
 
   const handleClose = () => {
-    setIsOpenModal(false)
+    dispatch(closeModal())
   }
 
   return (
-    <>
-      {/* Modal Search Start */}
-      <Modal
-        {...props}
-        id="searchModal"
-        tabIndex={-1}
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        aria-hidden="true"
-        className="modal-content rounded-0"
-        fullscreen
-        show={isOpenModal}
-        onHide={handleClose}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <h5 id="contained-modal-title-vcenter">
-              Search by keyword
-            </h5>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="d-flex align-items-center">
-          <div className="input-group w-75 mx-auto d-flex">
-            <input
-              type="search"
-              className="form-control p-3"
-              placeholder="keywords"
-              aria-describedby="search-icon-1"
-            />
-            <span id="search-icon-1" className="input-group-text p-3">
-              <i className="fa fa-search" />
-            </span>
-          </div>
-        </Modal.Body>
-      </Modal>
-      {/* Modal Search End */}
-    </>
+    <Modal
+      id="searchModal"
+      tabIndex={-1}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      aria-hidden="true"
+      className="modal-content rounded-0"
+      fullscreen
+      show={isOpenModal}
+      onHide={handleClose}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <h5 id="contained-modal-title-vcenter">Search by keyword</h5>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="d-flex align-items-center justify-content-center">
+        <div className="input-group w-75 mx-auto">
+          <input
+            type="search"
+            className="form-control p-3"
+            placeholder="keywords"
+            aria-describedby="search-icon-1"
+          />
+          <span id="search-icon-1" className="input-group-text p-3">
+            <i className="fa fa-search" />
+          </span>
+        </div>
+      </Modal.Body>
+    </Modal>
   )
 }
 
